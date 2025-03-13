c0111e1720b8f6583a391bac725f51a36c2d1a89
srlsrx <nicolasin97@gmail.com>
2025-03-13 18:26:58 +0100

    fix responsive on adopt component

--- src/pages/Adopt/Adopt.jsx
+++ src/pages/Adopt/Adopt.jsx
@@ -4,10 +4,10 @@ import constructionAnimation from "../../assets/construction-animation.json";
 
 const Adopt = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100 w-full">
+    <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100 w-full overflow-hidden">
       <Lottie animationData={constructionAnimation} className="w-[600px] mb-[-90px] mt-[-100px]" />
-      <h1 className="text-6xl font-bold text-[#317298]">Página en Construcción</h1>
-      <p className="text-3xl text-gray-700 max-w-[600px]">
+      <h1 className="text-[30px] font-bold text-[#317298] md:text-5xl lg:text-7xl">Página en Construcción</h1>
+      <p className="text-xl mt-0 text-gray-700 max-w-[600px] md:text-3xl lg:text-4xl lg:mt-4">
         Estamos trabajando para ofrecerte una mejor experiencia. ¡Vuelve pronto!
       </p>
     </div>
