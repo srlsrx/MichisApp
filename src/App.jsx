63bc42f5e796a97f94fe234180ef908983008b5f
Algol95 <angelaragon.developer@gmail.com>
2025-03-13 15:29:36 +0100

    Upd merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
-import  Home  from './pages/Home/Home'
-import  Adopt  from './pages/Adopt/Adopt'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Adopt from './pages/Adopt/Adopt'
 import './App.css'
 
+/** 
+ * Route asocia una URL con cada componente 
+ * @async
+ * @returns
+ * @author {Ana Castro}
+ */
 
 function App() { 
 
