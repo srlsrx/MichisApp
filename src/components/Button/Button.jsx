efa129e41758183119d3b486d8f5dcea6b03d671
srlsrx <nicolasin97@gmail.com>
2025-03-14 10:04:15 +0100

    feat cursor pointer on buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -14,7 +14,7 @@ const Button = () => {
         navigate("/Adopt")
     }
   return (    
-    <div><button onClick={goToAdopt} className="mt-4 bg-[#e25f23] text-white px-4 py-2 mb-8 rounded-full shadow-md hover:bg-blue-600 transition">Adoptame</button></div>
+    <div><button onClick={goToAdopt} className="mt-4  cursor-pointer bg-[#e25f23] text-white px-4 py-2 mb-8 rounded-full shadow-md hover:bg-blue-600 transition">Adoptame</button></div>
   )
 }
 
