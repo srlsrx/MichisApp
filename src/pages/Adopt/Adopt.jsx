6c608e18817b24df23063e423b75013bc8b74040
srlsrx <nicolasin97@gmail.com>
2025-03-14 11:45:25 +0100

    fix body bg-color and animation color

--- src/pages/Adopt/Adopt.jsx
+++ src/pages/Adopt/Adopt.jsx
@@ -21,7 +21,7 @@ const Adopt = () => {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-190px)] text-center bg-gray-100 px-6 w-full">
       <Lottie animationData={constructionAnimation} className="w-[380px] md:w-[550px] mb-[-50px] mt-[-100px]" />
-      <h1 className="text-[30px] font-bold text-[#317298] md:text-5xl lg:text-7xl">Página en Construcción</h1>
+      <h1 className="text-[30px] font-bold text-[#44B8A7] md:text-5xl lg:text-7xl">Página en Construcción</h1>
       <p className="text-xl mt-0 text-gray-700 max-w-[600px] md:text-3xl lg:text-4xl lg:mt-4">
         Estamos trabajando para ofrecerte una mejor experiencia. ¡Vuelve pronto!
       </p>
