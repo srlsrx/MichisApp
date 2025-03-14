1582c61141796f099d765b25e114d917f808709c
cuyssi <anabcastrolopez@gmail.com>
2025-03-14 16:08:38 +0100

    fix(MIC-18)adjust button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -14,7 +14,7 @@ const Button = () => {
         navigate("/Adopt")
     }
   return (    
-    <div><button onClick={goToAdopt} className="mt-4 bg-teal-500 cursor-pointer text-white px-4 py-2 mb-8 rounded-full shadow-md hover:bg-green-400 transition">Adoptame</button></div>
+    <div><button onClick={goToAdopt} className="mt-8 bg-teal-500 cursor-pointer text-white px-4 py-2 mb-8 rounded-full shadow-md hover:bg-green-400 transition">Adoptame</button></div>
   )
 }
 
--- src/components/CatCard/CatCard.jsx
+++ src/components/CatCard/CatCard.jsx
@@ -13,8 +13,8 @@ const CatCard = ({id, url}) => {
     <article className="card-container shadow-lg shadow-blue-500/40 ring-2 ring-green-400/20 rounded-2xl overflow-hidden w-80 h-[100%] transform transition duration-300 hover:scale-105" id={id}>
         <img className="w-full h-55 object-cover" src={url} alt="Gatito en adopción"/>
         <div className="card-body p-4 text-center bg-gray-200 ">
-            <p className="text-gray-700 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ullamcorper sapien.</p>
-            <Button  />
+            <p className="mt-4 text-gray-700 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ullamcorper sapien.</p>
+            <Button />
         </div>
     </article>
   )
