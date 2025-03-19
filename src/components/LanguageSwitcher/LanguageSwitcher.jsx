ae0d6bf0cba4b004c40e1dedcd944aa05191fbe3
srlsrx <nicolasin97@gmail.com>
2025-03-19 13:50:33 +0100

    fix flags to all devices

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import Flag from "react-world-flags";
 
 function LanguageSwitcher() {
     const { i18n } = useTranslation();
@@ -9,8 +10,8 @@ function LanguageSwitcher() {
     };
 
     return (
-        <button onClick={changeLanguage} className="cursor-pointer ring-1 bg-[#4FC560] ring-white dark:ring-gray-900 px-1 py-0 text-4xl rounded-lg absolute top-25 right-1">
-            {i18n.language === "es" ? "🇬🇧":"🇪🇸"}
+        <button onClick={changeLanguage} className="cursor-pointer ring-1 bg-[#4FC560] ring-white dark:ring-gray-900 px-2 py-2 text-4xl rounded-lg absolute top-25 right-1">
+            <Flag code={i18n.language === "es" ? "gb" : "es"} style={{ width: "30px", height: "20px" }} />
         </button>
     );
 }
