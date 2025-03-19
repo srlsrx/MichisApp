816725a8f7b2b72979bc9b737b98c5d757e03107
srlsrx <nicolasin97@gmail.com>
2025-03-19 11:41:19 +0100

    fix adopt page

--- src/pages/Adopt/Adopt.jsx
+++ src/pages/Adopt/Adopt.jsx
@@ -19,7 +19,7 @@ import constructionAnimation from "../../assets/construction-animation.json";
  */
 const Adopt = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-190px)] text-center bg-transparent px-6 w-full">
+    <div className="flex flex-col items-center justify-center h-full text-center bg-transparent px-6 w-full">
       <Lottie animationData={constructionAnimation} className="w-[380px] md:w-[550px] mb-[-50px] mt-[-100px]" />
       <h1 className="text-[30px] font-bold text-[#44B8A7] md:text-5xl lg:text-7xl">Página en Construcción</h1>
       <p className="text-xl mt-0 dark:text-gray-200 text-gray-700 max-w-[600px] md:text-3xl lg:text-4xl lg:mt-4">
