666fa419a56813763dd4138ba27809906f4d1bbf
Algol95 <angelaragon.developer@gmail.com>
2025-03-25 18:34:06 +0100

    release(MIC-44):Change button reusable

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useTranslation } from "react-i18next";
+import PropTypes from "prop-types";
 
-/**
- * @async
- * @returns {JSX.element}
- * @author {Ana Castro}
- */
-
-const Button = () => {
-  const { t } = useTranslation();
-
-    const navigate = useNavigate();
-    
-    const goToAdopt = () => {
-        navigate("/Adopt")
-    }
-  return (    
-    <div><button onClick={goToAdopt} className="bg-teal-500 justify-self-end cursor-pointer text-white px-4 py-2 mb-4 mt-4 rounded-full shadow-md hover:bg-green-400 transition">{t("adopt_me")}</button></div>
-  )
+function Button({
+  type = "button",
+  text,
+  className,
+  onClick,
+  bgColor = "bg-teal-500",
+  bgColorHover = "hover:bg-green-400",
+  txtColor = "text-white",
+}) {
+  return (
+    <button
+      type={type}
+      className={`${bgColor} ${bgColorHover} ${txtColor} ${className} justify-self-end cursor-pointer text-white px-4 py-2 m-4 rounded-full shadow-md transition`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
 
-export default Button
+Button.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
